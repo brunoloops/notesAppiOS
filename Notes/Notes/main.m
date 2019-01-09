cc6ade7cadbb2a95643bc22e229b15f6f55df015
Bruno <bruno.vescovi00@gmail.com>
2019-01-09 10:28:58 -0300

    delete singleton on main

--- Notes/Notes/main.m
+++ Notes/Notes/main.m
@@ -8,11 +8,9 @@
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
-#import "DataManager.h"
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
-        DataManager *dataManager = [DataManager sharedManager];
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
